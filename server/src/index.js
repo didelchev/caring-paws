bb30d06b176fe2d0056f0a3218951c0a15ac4237
Daniel Delchev <daniel.delchev@yahoo.com>
2025-09-13 16:46:02 +0300

    add home controller

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import routes from './routes.js';
 
 
 const app = express();
 
 const PORT = 5000;
+
+
+app.use(routes)
 
 app.listen(PORT, () => {
     console.log('Server is listening on http://localhost:5000 ...')
