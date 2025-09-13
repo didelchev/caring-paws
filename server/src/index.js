942b08b47631ec146d476860e4867dbb1b5c9259
Daniel Delchev <daniel.delchev@yahoo.com>
2025-09-13 17:46:25 +0300

    add cors & express json

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import routes from './routes.js';
+import cors from 'cors'
+
 
 
 const app = express();
+
+app.use(cors())
+
+app.use(express.json())
+
 
 const PORT = 5000;
 
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import homeController from "./controllers/homeController.js";
 import catalogController from "./controllers/catalogController.js";
+import authController from "./controllers/authController.js";
 
 const routes = Router();
 
 routes.use('/', homeController)
 
 routes.use('/catalog', catalogController)
+
+routes.use('/auth', authController)
 
 
 export default routes;
