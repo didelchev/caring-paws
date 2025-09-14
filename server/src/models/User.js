320df8432709b53827590f67682729ccaf4bdbdc
Daniel Delchev <daniel.delchev@yahoo.com>
2025-09-14 23:13:42 +0300

    add user schema

---
+++ server/src/models/User.js
@@ -0,0 +1,17 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+         minLength: [6, "Your password is too short !"]
+    }
+})
