73305532e218675f08338c75fa0cab02de0386eb
Daniel Delchev <daniel.delchev@yahoo.com>
2025-09-18 10:39:09 +0300

    add error handling to register endpoint

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,14 +1,22 @@
 import { Router } from "express";
+import userService from "../services/userService.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
 
 
 const userController = Router();
 
 userController.post('/register', async (req, res) => {
-    const { email, password } = req.body
+    const { email, password, username } = req.body
 
-    const result = await userService.register(email, password);
+    try {
+        const result = await userService.register(email,username, password)
+        res.json(result);
+        
+    } catch (err) {
+        res.status(400).json({message: getErrorMessage(err)})
+    }
 
-    res.json(result);
+    
 });
 
 userController.post('/login', async (req, res) => {
