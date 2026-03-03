c2825763550dc36489a5ebab7002bb0d5328a7f9
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-04 01:58:44 +0200

    update routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,16 +3,20 @@ import homeController from "./controllers/homeController.js";
 import userController from "./controllers/userController.js";
 import dogController from "./controllers/dogController.js";
 import commentController from "./controllers/commentController.js";
+import favoriteController from "./controllers/favoriteController.js";
+import dashboardController from "./controllers/dashboardController.js";
 
 const routes = Router();
 
-routes.use('/', homeController)
+routes.use("/", homeController);
+routes.use("/petcatalog", dogController);
+routes.use("/auth", userController);
 
-routes.use('/petcatalog', dogController)
+// Nested routes for comments and favorites per dog
+routes.use("/petcatalog/:dogId/comments", commentController);
+routes.use("/petcatalog/:dogId/favorite", favoriteController);
 
-routes.use('/auth', userController)
-
-routes.use("/petcatalog/:dogId/comments", commentController)
-
+// Dashboard
+routes.use("/dashboard", dashboardController);
 
 export default routes;
