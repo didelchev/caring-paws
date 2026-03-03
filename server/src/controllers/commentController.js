537984980a82ea71bc94f9877f555964a947fbbf
Daniel Delchev <daniel.delchev@yahoo.com>
2026-03-04 01:45:15 +0200

    add comments controller

---
+++ server/src/controllers/commentController.js
@@ -0,0 +1,47 @@
+import { Router } from "express";
+import commentService from "../services/commentService.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
+
+const commentController = Router({ mergeParams: true });
+
+// GET /petcatalog/:dogId/comments
+commentController.get("/", async (req, res) => {
+  try {
+    const comments = await commentService.getByDog(req.params.dogId);
+    res.json(comments);
+  } catch (err) {
+    res.status(400).json({ message: getErrorMessage(err) });
+  }
+});
+
+// POST /petcatalog/:dogId/comments
+commentController.post("/", async (req, res) => {
+  if (!req.user) return res.status(401).json({ message: "Unauthorized" });
+
+  const { text } = req.body;
+  const { dogId } = req.params;
+  const userId = req.user._id;
+  const ownerUsername = req.user.username || req.user.email;
+
+  try {
+    const comment = await commentService.create(text, dogId, userId, ownerUsername);
+    res.status(201).json(comment);
+  } catch (err) {
+    res.status(400).json({ message: getErrorMessage(err) });
+  }
+});
+
+// DELETE /petcatalog/:dogId/comments/:commentId
+commentController.delete("/:commentId", async (req, res) => {
+  if (!req.user) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const deleted = await commentService.delete(req.params.commentId, req.user._id);
+    if (!deleted) return res.status(403).json({ message: "Not allowed" });
+    res.status(204).end();
+  } catch (err) {
+    res.status(400).json({ message: getErrorMessage(err) });
+  }
+});
+
+export default commentController;
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import homeController from "./controllers/homeController.js";
 import userController from "./controllers/userController.js";
 import dogController from "./controllers/dogController.js";
+import commentController from "./controllers/commentController.js";
 
 const routes = Router();
 
@@ -10,6 +11,8 @@ routes.use('/', homeController)
 routes.use('/petcatalog', dogController)
 
 routes.use('/auth', userController)
+
+routes.use("/petcatalog/:dogId/comments", commentController)
 
 
 export default routes;
